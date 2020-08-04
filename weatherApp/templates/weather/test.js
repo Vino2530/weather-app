4f44db1134d8395cbf96eaba2ad0af755f243448
arsha <sajevinoth@gmail.com>
2020-08-04 19:43:57 +0530

    initial commit

---
+++ weatherApp/templates/weather/test.js
@@ -0,0 +1,10 @@
+alert('test');
+
+var date = new Date()
+        document.getElementById("date").innerHTML = date;
+
+        function searchLocation() {
+            let placeName = document.getElementById('placeName').value;
+            alert(placeName)
+            console.log(placeName);
+        }
